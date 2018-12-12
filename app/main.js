be2f8422e7f988510f3df457b2f9be341d1faa24
loves9 <ifengfree@gmail.com>
2018-12-12 17:04:55 +0800

    [+]react-native-elements react-native-vector-icons

--- app/main.js
+++ app/main.js
@@ -1,18 +1,24 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import React from "react";
+import { Text, View } from "react-native";
 import {
     createBottomTabNavigator,
     createAppContainer,
     createStackNavigator,
     StackNavigator
 } from "react-navigation";
-
-import HomeView from './HomeView'
+import Ionicons from "react-native-vector-icons/Ionicons";
+import HomeView from "./HomeView";
 
 class HomeScreen extends React.Component {
     render() {
         return (
-            <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <View
+                style={{
+                    flex: 1,
+                    justifyContent: "center",
+                    alignItems: "center"
+                }}
+            >
                 <Text>Home!</Text>
             </View>
         );
@@ -21,12 +27,18 @@ class HomeScreen extends React.Component {
 
 class SettingsScreen extends React.Component {
     static navigationOptions = {
-        title: 'Setting',
+        title: "Setting"
     };
 
     render() {
         return (
-            <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <View
+                style={{
+                    flex: 1,
+                    justifyContent: "center",
+                    alignItems: "center"
+                }}
+            >
                 <Text>Settings!</Text>
             </View>
         );
@@ -35,7 +47,7 @@ class SettingsScreen extends React.Component {
 
 class A extends React.Component {
     static navigationOptions = {
-        tabBarLabel: 'Home!',
+        tabBarLabel: "Home!"
     };
 
     // etc..
@@ -43,7 +55,7 @@ class A extends React.Component {
 
 class B extends React.Component {
     static navigationOptions = {
-        tabBarLabel: 'Settings!',
+        tabBarLabel: "Settings!"
     };
 
     // etc..
@@ -52,15 +64,45 @@ class B extends React.Component {
 const HomeStack = createStackNavigator({ Home: HomeView });
 const SettingsStack = createStackNavigator({ Settings: SettingsScreen });
 
-
 const TabNavigator = createBottomTabNavigator({
     Home: HomeView,
     Settings: SettingsScreen
 });
 
 export default createAppContainer(
-    createBottomTabNavigator({
-        Home: HomeStack,
-        Setting: SettingsStack,
-    })
+    createBottomTabNavigator(
+        {
+            Home: HomeStack,
+            Setting: SettingsStack
+        },
+        {
+            defaultNavigationOptions: ({ navigation }) => ({
+                tabBarIcon: ({ focused, horizontal, tintColor }) => {
+                    const { routeName } = navigation.state;
+                    let iconName;
+                    if (routeName === "Home") {
+                        iconName = `ios-information-circle${
+                            focused ? "" : "-outline"
+                        }`;
+                    } else if (routeName === "Setting") {
+                        iconName = `ios-contact${focused ? "" : "-outline"}`;
+                    }
+
+                    // You can return any component that you like here! We usually use an
+                    // icon component from react-native-vector-icons
+                    return (
+                        <Ionicons
+                            name={iconName}
+                            size={horizontal ? 20 : 25}
+                            color={tintColor}
+                        />
+                    );
+                }
+            }),
+            tabBarOptions: {
+                activeTintColor: "tomato",
+                inactiveTintColor: "gray"
+            }
+        }
+    )
 );
